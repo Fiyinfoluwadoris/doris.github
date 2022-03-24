4eefa96073c54bb6684d29401b78eb13d20cfcfc
Fiyinfoluwadoris <100928884+Fiyinfoluwadoris@users.noreply.github.com>
2022-03-24 04:09:03 -0700

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,34 @@
+//subtraction of 4 numbers
+
+#include<iostream>
+int main()
+
+{
+
+ 
+	//variable declaration 
+	
+	int p=0;
+	int r=0;
+	int t=0;
+	int a=0;
+	float s1=0;
+	
+	std::cout<<"enter the first number\n";
+	std::cin>>p;
+	
+	std::cout<<"enter the second number\n";
+	std::cin>>r;
+	
+		std::cout<<"enter the third number\n";
+	std::cin>>t;
+	
+		std::cout<<"enter the fourth number\n";
+	std::cin>>a;
+	
+	s1=p*r*t*a-;
+	
+	
+	std::cout<<s1;
+	return 0;
+}
